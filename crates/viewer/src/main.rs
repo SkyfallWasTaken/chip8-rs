2a8b0441898625a8c9cd001d9f1ab42c959ee38b
SkyfallWasTaken <mahadkalam1@proton.me>
2024-04-04 13:21:56 +0100

    Add color_eyre and clap

--- crates/viewer/src/main.rs
+++ crates/viewer/src/main.rs
@@ -1,19 +1,34 @@
-use std::env;
 use std::fs;
+use std::path::PathBuf;
+
+use color_eyre::{eyre::WrapErr, Result};
 
 use machine::{Machine, Quirks, SCREEN_HEIGHT, SCREEN_WIDTH};
 use macroquad::prelude::*;
 
 const SCALE_FACTOR: i32 = 10;
 
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// The path to the CHIP-8 ROM.
+    path: PathBuf,
+
+    /// Logs debugging information after this cycle is executed
+    #[arg(long, short)]
+    cycle_to_log: Option<u32>,
+}
+
 #[macroquad::main("CHIP-8 Emulator")]
-async fn main() {
+async fn main() -> Result<()> {
+    color_eyre::install()?;
     env_logger::init();
 
-    let path = env::args().nth(1).expect("No path specified");
-    let cycle_to_log = env::args().nth(2).map(|n| n.parse::<u32>().unwrap());
+    let cli = Cli::parse();
 
-    let rom = fs::read(path).unwrap();
+    let rom = fs::read(cli.path).wrap_err("Failed to read ROM")?;
     let mut machine = Machine::from_rom(&rom, Quirks::modern());
 
     let mut current_cycle = 1;
@@ -21,11 +36,11 @@ async fn main() {
         machine.cycle();
         current_cycle += 1;
 
-        if is_key_down(KeyCode::Right) {
+        if is_key_released(KeyCode::Right) {
             log_debug_info(&machine, current_cycle);
         }
 
-        if let Some(cycle_to_log) = cycle_to_log {
+        if let Some(cycle_to_log) = cli.cycle_to_log {
             if cycle_to_log == current_cycle {
                 log_debug_info(&machine, current_cycle);
             }
